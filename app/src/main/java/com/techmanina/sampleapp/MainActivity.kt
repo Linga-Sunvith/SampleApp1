f58df4fa9227af9629bb2d42b64ea05b5eeef0af
Manoj8080 <uisloading@gmail.com>
2025-10-21 17:17:57 +0530

    manoj

--- app/src/main/java/com/techmanina/sampleapp/MainActivity.kt
+++ app/src/main/java/com/techmanina/sampleapp/MainActivity.kt
@@ -2,17 +2,34 @@ package com.techmanina.sampleapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var login : Button;
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        login = findViewById(R.id.login)
+
+        login.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linear)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
